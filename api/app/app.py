3d012ba756b2089ecda47a2fc6ceb526e95be17f
osawa-koki <github@osawa.dev>
2023-04-06 03:08:34 +0900

    応答基本処理。

--- api/app/app.py
+++ api/app/app.py
@@ -1,8 +1,15 @@
 import json
 import logging
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+SECRET = os.environ.get("SECRET")
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 
 def ping(event, context):
     return {
@@ -34,13 +41,32 @@ def main(event, context):
     # ボディにtypeが含まれていて、url_verificationの場合は200を返す
     # SlackのEvent APIのURL Verificationのための処理
     if "type" in body and body["type"] == "url_verification":
+        secret = event["queryStringParameters"]["secret"]
+        if secret != SECRET:
+            return {
+                "statusCode": 403,
+                "body": json.dumps(
+                    {
+                        "message": "Forbidden",
+                    }
+                ),
+            }
         return {
             "statusCode": 200,
             "body": json.dumps(body),
         }
 
     # ここからメイン処理！
-    logger.info(body)
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/www-form-urlencoded",
+    }
+    form_data = {
+        "token": SLACK_TOKEN,
+        "channel": body["event"]["channel"],
+        "text": "Hello World!",
+    }
+    requests.post(url, headers=headers, data=form_data)
 
     return {
         "statusCode": 200,
