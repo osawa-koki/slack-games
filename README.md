a4fc05f45240db9f2d9bfb86663d5723d263593b
osawa-koki <github@osawa.dev>
2023-04-06 00:57:51 +0900

    全体的な修正。

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # slack-games
 
 🍺🍺🍺 Slack上で遊べるゲームを作ってみた！  
+
+## 開発用実行
+
+```shell
+cd ./api
+sam build --use-container ;; sam local start-api
+```
---
+++ api/app/app.py
@@ -0,0 +1,11 @@
+import json
+
+def ping(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "pong",
+            }
+        ),
+    }
