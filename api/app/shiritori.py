28e7d27a1e34fcf73db9b43a16d5d5ca7724beaf
osawa-koki <github@osawa.dev>
2023-04-07 02:03:08 +0900

    終了時に全てを表示。

--- api/app/shiritori.py
+++ api/app/shiritori.py
@@ -36,16 +36,18 @@ def make_action(channel_id, user, text, item_python_dict):
         last_word = words[-1]
         if last_word[-1] != text[0]:
             terminate_shiritori(channel_id)
+            words_str = " -> ".join(words)
             return {
                 "result": -1,
-                "message": f"「{last_word}」の後ろに「{text}」はつけられません。",
+                "message": f"「{last_word}」の後ろに「{text}」はつけられません。\n\n{words_str}",
             }
 
         if text in words:
             terminate_shiritori(channel_id)
+            words_str = " -> ".join(words)
             return {
                 "result": -1,
-                "message": f"「{text}」は既に使われています。",
+                "message": f"「{text}」は既に使われています。\n\n{words_str}",
             }
 
         words.append(text)
